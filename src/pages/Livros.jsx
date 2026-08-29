d1925a3d9c3c68b2efbe2f574d94f566d60635d8
Mavi <mavisberrye@gmail.com>
2026-08-29 11:48:07 -0300

    Feat: adição da seção Livros

--- src/pages/Livros.jsx
+++ src/pages/Livros.jsx
@@ -1,9 +1,42 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import imgCrimeECastigo from '/src/pages/imagens/crimeecastigo.jpg'
+import imgRevolucaoBichos from '/src/pages/imagens/revolucaodosbichos.webp'
+import imgPrincipeCruel from '/src/pages/imagens/principecruel.jpg'
+
 function Livros() {
+  const [livrosExemplo] = useState([
+    { id: 1, titulo: 'Crime e Castigo', autor: 'Fiódor Dostoiévski', genero: 'Clássico', doador: 'Ana', imagem: imgCrimeECastigo },
+    { id: 2, titulo: 'A Revolução dos Bichos', autor: 'George Orwell', genero: 'Fábula', doador: 'Carlos', imagem: imgRevolucaoBichos },
+    { id: 3, titulo: 'O Príncipe Cruel', autor: 'Holly Black', genero: 'Fantasia', doador: 'Mariana', imagem: imgPrincipeCruel },
+  ])
+
   return (
-    <div>
-      <h1>Livros</h1>
-      <p>Livros disponíveis para troca.</p>
-    </div>
+    <main className="livros-container">
+      <div className="livros-header">
+        <div>
+          <h1>Livros Disponíveis</h1>
+          <p>Encontre histórias para trocar e novos conhecimentos para cultivar.</p>
+        </div>
+      </div>
+
+      <section className="livros-grid">
+        {livrosExemplo.map((livro) => (
+          <div key={livro.id} className="livro-card">
+            <div className="card-capa">
+              <img src={livro.imagem} alt={`Capa do livro ${livro.titulo}`} />
+            </div>
+            <div className="card-info">
+              <span className="card-genero">{livro.genero}</span>
+              <h3>{livro.titulo}</h3>
+              <p className="card-autor">Por: {livro.autor}</p>
+              <p className="card-doador"> Com: <strong>{livro.doador}</strong></p>
+              <button className="botao-secundario-pequeno">Tenho Interesse</button>
+            </div>
+          </div>
+        ))}
+      </section>
+    </main>
   )
 }
 
