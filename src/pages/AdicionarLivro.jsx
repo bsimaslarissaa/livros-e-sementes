4fbac62cce61ef3ae6ea762138901e273dbbf6be
Mavi <mavisberrye@gmail.com>
2026-09-02 18:52:24 -0300

    iniciando a implementação do banco de dados

--- src/pages/AdicionarLivro.jsx
+++ src/pages/AdicionarLivro.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { supabase } from '../supabaseClient'
 
 function AdicionarLivro() {
   const navigate = useNavigate()
