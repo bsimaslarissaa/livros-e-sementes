dc4068dd95e9256d443c158c6831d530b87b3c64
Mavi <mavisberrye@gmail.com>
2026-09-05 10:34:09 -0300

    Structure: Separação do CSS da página Home em um arquivo próprio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
+import './pages/CSS/App.css'
+import './pages/CSS/Home.css'
 
 import Header from './components/Header'
 import Home from './pages/Home'
